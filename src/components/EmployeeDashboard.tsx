d1cb44b7154852a2e0b4cc94ff08533ead2588da
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-08-28 13:00:16 +0530

    Updated EmployeeDashboard.tsx

--- src/components/EmployeeDashboard.tsx
+++ src/components/EmployeeDashboard.tsx
@@ -1128,6 +1128,7 @@ export const EmployeeDashboard: React.FC<EmployeeDashboardProps> = ({ onBack }) 
                     value={calculateRetirementDate(formData.date_of_birth, formData.cadre)?.toISOString().split('T')[0] || ''}
                     readOnly
                     className="w-full px-3 py-2 border border-gray-300 rounded-lg bg-gray-100 text-gray-500 cursor-not-allowed"
+                    title="Retirement date is auto-calculated based on date of birth and Cadre"
                     disabled
                   />
                 </div>
