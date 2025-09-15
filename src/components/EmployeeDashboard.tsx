dc7773debc7ed475b907be06ee33a989fff8a938
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-09-15 23:14:09 +0530

    Updated EmployeeDashboard.tsx

--- src/components/EmployeeDashboard.tsx
+++ src/components/EmployeeDashboard.tsx
@@ -1138,7 +1138,7 @@ export const EmployeeDashboard: React.FC<EmployeeDashboardProps> = ({ onBack }) 
         </div>
       </div>
 
-      {/* Add Employee Modal */}
+{/* Add Employee Modal */}
       {showAddModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-xl max-w-4xl w-full mx-4 max-h-[90vh] overflow-y-auto">
