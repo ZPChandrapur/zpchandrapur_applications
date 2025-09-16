d3b2eeb85620977e81b6d4a35db667a75718894a
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-09-17 02:02:52 +0530

    Updated EducationEmployeeDashboard.tsx

--- src/components/EducationEmployeeDashboard.tsx
+++ src/components/EducationEmployeeDashboard.tsx
@@ -183,7 +183,7 @@ export const EducationEmployeeDashboard: React.FC<EducationEmployeeDashboardProp
         .select('*')
         .eq('dept_id', educationDeptId) // Filter for Education Department
         .limit(5000)
-        //.range(0, count ? count - 1 : 4999) // Fetch all records or up to 5000
+        .range(0, count ? count - 1 : 7000) // Fetch all records or up to 5000
         .order('employee_name');
       
       if (error) throw error;
