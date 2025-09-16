971adaf89a254580d0e0fceba1ae04dafc906325
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-09-17 02:29:08 +0530

    Updated EmployeeDashboard.tsx

--- src/components/EmployeeDashboard.tsx
+++ src/components/EmployeeDashboard.tsx
@@ -470,7 +470,6 @@ export const EmployeeDashboard: React.FC<EmployeeDashboardProps> = ({ onBack }) 
           panchayatrajsevarth_id,
           ddo_code,
           Cadre,
-          working_office_name,
           date_of_joining,
           created_at,
           updated_at
