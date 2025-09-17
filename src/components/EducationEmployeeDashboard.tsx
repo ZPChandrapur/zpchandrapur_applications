3a5426db643c9ff74799197fc7a39fd8685ced76
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-09-18 01:09:34 +0530

    Updated EducationEmployeeDashboard.tsx

--- src/components/EducationEmployeeDashboard.tsx
+++ src/components/EducationEmployeeDashboard.tsx
@@ -892,8 +892,8 @@ export const EducationEmployeeDashboard: React.FC<EducationEmployeeDashboardProp
                   >
                     <option value="">{t('erms.selectDesignation')}</option>
                     {designations.map(designation => (
-                      <option key={designations.designation_id} value={designation.designation}>
-                        {designation.designation}
+                      <option key={designations.designation_id} value={designations.designation}>
+                        {designations.designation}
                       </option>
                     ))}
                   </select>
