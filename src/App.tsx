05e3fbe5fabf557ccbde702dc1c7bbeec6f1e4bb
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-09-29 19:59:32 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { Camera } from 'lucide-react';
 import { SignInForm } from './SignInForm';
 import { Dashboard } from './components/Dashboard';
-import { supabase, isSupabaseConfigured, supabaseConfigErrors } from './lib';
+import { supabase, isSupabaseConfigured, supabaseConfigErrors } from 'src/lib/supabase.ts';
 import type { User } from '@supabase/supabase-js';
 
 function App() {
